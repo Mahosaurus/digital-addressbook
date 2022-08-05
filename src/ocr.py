6fe77213b5b3e93451c43ef83ab860616b827870
Malte Hoffmann <malte.hoffmann@siemensgamesa.com>
2022-08-05 12:36:32 +0000

    better handling of data

--- src/ocr.py
+++ src/ocr.py
@@ -1,8 +1,8 @@
 """ OCR module """
+import json
 import os
+import re
 import sys
-
-from typing import Iterator
 
 from PIL import Image
 from pdf2image import convert_from_path
@@ -12,33 +12,56 @@ def main(input_folder: str):
     for root, _, files in os.walk(input_folder):
         for file in files:
             file_cand = os.path.join(root, file)
-            if os.path.isfile(file_cand):
+            if os.path.isfile(file_cand) and "output" not in file_cand:
                 output_path = get_output_path(file_cand, input_folder)
                 do_ocr(file_cand, output_path)
- 
+
 def get_output_path(input_path: str, input_folder: str) -> str:
+    """ TODO: Always go down with root path till only one / is left"""
+    while len(input_folder.split("/")) > 2: # TODO: make universal for every OS
+        input_folder = os.path.dirname(input_folder)
     abs_output_path = os.path.abspath(input_path)
-    file_name = os.path.basename(abs_output_path)
+    file_name = os.path.basename(abs_output_path).split(".")[0]
     folder_name = os.path.dirname(abs_output_path)
     common_path = os.path.commonpath([os.path.abspath(input_folder), folder_name])
     root_path = os.path.dirname(common_path)
     subdir = os.path.relpath(folder_name, common_path)
     final_path = os.path.join(root_path, "output", subdir, file_name)
     os.makedirs(os.path.join(root_path, "output", subdir), exist_ok=True)
+    print("IP:", input_path)
+    print(input_folder)
+    print(abs_output_path)
+    print(folder_name)
+    print("CP:", common_path)
+    print(root_path)
+    print(subdir)
+    print(final_path)
     return final_path
-
 
 def do_ocr(file_cand, output_path):
     pages = convert_from_path(file_cand, 1000, single_file=True)
-    print("Starting tesseract")
-    #result = pytesseract.image_to_string(pages[0], , lang='ger')
-    # Get a searchable PDF  
-    pdf = pytesseract.image_to_pdf_or_hocr(pages[0], extension='pdf')
-    with open(output_path, 'w+b') as fhandle:
-        fhandle.write(pdf) # pdf type is bytes by default
+    print("Starting tesseract img to str")
+
+    # Get an indexable document
+    result_text = bytes(pytesseract.image_to_string(pages[0], lang='deu'), 'utf-8').decode('utf-8')
+    result_text = result_text.replace("\n", "")
+    result_text = result_text.replace(",", " ")
+    result_text = result_text.replace("-", " ")
+    result_text = result_text.replace("—", " ")
+    result_text = result_text.replace("—", " ")
+    result_text = [text.lower() for text in result_text.split(" ") if text]
+
+    with open(output_path + ".json", "w", encoding='utf-8') as fhandle:
+        json.dump(result_text, fhandle, ensure_ascii=False)
+
+    print("Starting tesseract img to pdf")
+    # Get a searchable PDF
+    result_pdf = pytesseract.image_to_pdf_or_hocr(pages[0], lang='deu', extension='pdf')
+    with open(output_path + ".pdf", 'w+b') as fhandle:
+        fhandle.write(result_pdf)
 
 
 if __name__ == "__main__":
     input_folder = sys.argv[1]
     main(input_folder)
-    
+
